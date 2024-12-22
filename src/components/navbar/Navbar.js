69b9932bf6b6d062cbac5f66a343ddebc95ecc17
rayxtn <houimliraed@outlook.fr>
2024-12-22 19:31:26 +0100

    navbar

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className='navbar'>
+    <div className='logo'>
+    <h2>Beaches</h2>
+
+    </div>
+      
+    </div>
+  )
+}
+
+export default Navbar
